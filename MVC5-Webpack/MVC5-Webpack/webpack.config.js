46edf92baf4832732cf47ab04adb837b1d9b0178
JVB <1976577+jvb93@users.noreply.github.com>
2018-01-18 18:31:24 -0800

    bootstrap is go - module building works

--- MVC5-Webpack/MVC5-Webpack/webpack.config.js
+++ MVC5-Webpack/MVC5-Webpack/webpack.config.js
@@ -1,1 +1,19 @@
-﻿
+﻿var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'App'),
+    entry: './main.js',
+    output: {
+        path: path.join(__dirname, 'Build'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/, loader: "url" }
+        ]
+    }
+};
